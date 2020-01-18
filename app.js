5bcdf74b40d0f3c829e8d8411a31b3706ef7eec0
Fabiano Netto <fabianonetto@gmail.com>
2020-01-18 18:04:58 -0500

    Added chalk npm package and printed Success! in green

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
-
 console.log(msg)
 
-
-
-
-// const add = require('./utils.js')
-// const sum = add(4,6)
-// console.log(sum)
+console.log(chalk.green('Success!'))

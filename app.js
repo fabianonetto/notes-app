ed0e91dda5c4a5dc0a438eb05291c41f2417c5a5
Fabiano Netto <fabianonetto@gmail.com>
2020-01-18 18:08:13 -0500

    Played around with chalk package

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk')
-const getNotes = require('./notes.js')
+// const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// const msg = getNotes()
+// console.log(msg)
 
-console.log(chalk.green('Success!'))
+const msg = 'Success!'
+
+console.log(chalk.green(msg))
+console.log(chalk.bold(msg))
+console.log(chalk.inverse(msg))
+console.log(chalk.green.inverse(msg))

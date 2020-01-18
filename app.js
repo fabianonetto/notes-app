b5e2c83f199373b5bda057635851f16b0eac8c41
Fabiano Netto <fabianonetto@gmail.com>
2020-01-18 16:54:05 -0500

    Imported validator npm package and used to validate an URL

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
 
-
+console.log(validator.isURL('https/mead.io'))
 
 
 // const add = require('./utils.js')

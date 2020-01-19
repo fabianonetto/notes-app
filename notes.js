2e4a607bd7071e0bd0b87fb15687d98997c6ccf8
Fabiano Netto <fabianonetto@gmail.com>
2020-01-19 17:00:51 -0500

    implement read command

--- notes.js
+++ notes.js
@@ -21,9 +21,9 @@ const removeNote = (title) => {
 const addNote = (title, body) => {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter((note) =>  note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0){
+    if (!duplicateNote){
         notes.push({
             title: title,
             body: body
@@ -58,9 +58,24 @@ const listNotes = () => {
     })
 }
 
+const readNote = (title) => {
+    const notes = loadNotes()
+
+    const noteFound = notes.find((note) => note.title === title)
+
+    if(noteFound){
+        console.log(chalk.green.bold(noteFound.title))
+        console.log(noteFound.body)
+    }
+    else {
+        console.log(chalk.red('Note not found'))
+    }
+}
+
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
-  listNotes: listNotes
+  listNotes: listNotes,
+  readNote: readNote
 };
